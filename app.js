4a44fffd8b3347dace5f1e55cec3779c1d08ffb9
vagton <vagton.ferreira@gmail.com>
2023-09-02 18:36:10 -0300

    Teste

---
+++ app.js
@@ -0,0 +1,17 @@
+let SimpleWords = ["oi", "ok"];
+
+for (const element of SimpleWords) {
+    console.log(element);
+}
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u1F47D'
+const teste = 'abcd'
+console.log('👽')
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+console.log(cifrao.length)
